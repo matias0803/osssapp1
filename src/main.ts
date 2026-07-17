f8000f601976bd57d1dfe9d7a74ab51d0c1d3a6f
matias0803 <matias0803@github.com>
2026-07-16 23:10:20 -0400

    Fix CORS explicitly allowing headers

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
