9b330145277fe8875afd882948826b8149c835a4
matias0803 <matias0803@github.com>
2026-07-25 01:24:29 -0400

    fix: close modal on delete and fix objetivo update validation

--- src/objetivos/dto/update-objetivo.dto.ts
+++ src/objetivos/dto/update-objetivo.dto.ts
@@ -1,5 +1,4 @@
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateObjetivoDto } from './create-objetivo.dto';
 
-export class UpdateObjetivoDto extends CreateObjetivoDto {
-  id?: string;
-}
+export class UpdateObjetivoDto extends PartialType(CreateObjetivoDto) {}
